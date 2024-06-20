5ffa79a03114c33cffd6e1907f6ab561df066049
AmarDodda <amardodda23@gmail.com>
2024-06-20 23:27:06 +0530

    CRM Frontend

--- src/services/instance.js
+++ src/services/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Defining the base URL for the API
-const baseURL = 'https://crm-backend-5-d8g1.onrender.com/';
+const baseURL = process.env.REACT_APP_API_BASE_URL;
 
 const instance = axios.create({
     baseURL,
